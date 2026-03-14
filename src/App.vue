a4243ef0c5e73a4ce4a3d2693a02dd29302aef0b
nikonasg <Gianitsos04@icloud.com>
2026-03-13 21:36:29 -0700

    Added blogpost and button counter files along with app changes

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <!--<ShoppingCart class="shopping-cart"></ShoppingCart>-->
+  <BlogPost
+    id="1"
+    blogPostTitle="What is a muggle?"
+    blogPostContent="A muggle is a person who lacks any sort of magical ability."
+    ></BlogPost>
+</template>
+
+<script setup>
+//import ShoppingCart from '@/components/ShoppingCart.vue'
+import BlogPost from './BlogPost.vue'
+</script>
+
+<style scoped>
+/* Styles for the shopping cart */
+.shopping-cart {
+  font-family: 'Arial', sans-serif;
+  background-color: #f8f8f8;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/BlogPost.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="blog-post">
+        <p>
+            {{  message }}
+        </p>
+
+        <h2>{{  id }} - {{  blogPostTitle }}</h2>
+        <h4> {{  blogPostContent }}</h4>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+let message = ref('This is the BlogPost component.')
+defineProps(['id', 'blogPostTitle', 'blogPostContent'])
+</script>
+
+<style scoped>
+.blog-post {
+    background-color: aqua;
+    padding: 10px;
+    margin-bottom: 10px;
+}
+
+</style>
